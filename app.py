00ebffaa69206b322ff78a8e89ad099a9d30061f
Jaime Daniel <jjjsouza2015@gmail.com>
2024-10-24 23:07:14 -0300

    finish data contract

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 from datetime import datetime, time
+from contrato import Vendas
+from pydantic import ValidationError
 
 def main():
 
@@ -13,14 +15,28 @@ def main():
 
     if st.button ("Salvar"):
 
-        data_hora = datetime.combine(data, hora)
-        st.write("**Dados da venda:**")
-        st.write(f"Email do vendedor: {email}")
-        st.write(f"Data e hora da compra: {data_hora}")
-        st.write(f"Valor da venda: R$ {valor:.2f}")
-        st.write(f"Quantidade de produtos: {quantidade}")
-        st.write(f"Produto: {produto}")
+            try:
+                data_hora = datetime.combine(data, hora)
 
-    
+                venda = Vendas(
+                    email = email,
+                    data = data,
+                    valor = valor,
+                    quantidade = quantidade,
+                    produto = produto
+                )
+
+                data_hora = datetime.combine(data, hora)
+                st.write("**Dados da venda:**")
+                st.write(f"Email do vendedor: {email}")
+                st.write(f"Data e hora da compra: {data_hora}")
+                st.write(f"Valor da venda: R$ {valor:.2f}")
+                st.write(f"Quantidade de produtos: {quantidade}")
+                st.write(f"Produto: {produto}")
+                st.write(venda)
+            except ValidationError as e:
+                st.error(f"Deu erro{e}")
+        
+
 if __name__ == "__main__":
     main()
--- contrato.py
+++ contrato.py
@@ -1,11 +1,17 @@
 from datetime import datetime
 from typing import Tuple
 
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, PositiveFloat, PositiveInt
+from enum import Enum 
+
+class ProdutoEnum (str, Enum):
+    produto1 = "ZapFlow com gemini"
+    produto2 = "ZapFlow com ChatGPT"
+    produto3 = "ZapFlow com Llama 3.0" 
 
 class Vendas(BaseModel):
     email: EmailStr 
     data: datetime
-    valor: float
-    quantidade: int
-    produto: str 
+    valor: PositiveFloat
+    quantidade: PositiveInt
+    produto: ProdutoEnum
